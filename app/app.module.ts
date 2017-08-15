72d3dc5d8f6bfa1d138b4dd91c5c931ca66c07a7
Nathan Randall <nathan.randall@TCC.local>
2017-08-15 14:00:40 -0400

    added toastr service

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,16 +5,23 @@ import { EventsAppComponent } from './events/events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component';
-
+import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   imports: [BrowserModule],
-  declarations: [
-    EventsAppComponent,
-    EventsListComponent,
-    EventThumbnailComponent,
-    NavBarComponent
-  ],
-  bootstrap: [EventsAppComponent]
+    declarations: [
+        EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        NavBarComponent
+    ],
+    providers: [
+        EventService,
+        ToastrService
+    ],
+    bootstrap: [
+        EventsAppComponent
+    ]
 })
 export class AppModule {}
