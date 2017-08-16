cbe9516890b8da646e607faba8399e7d3b2225cd
Nathan Randall <nathan.randall@TCC.local>
2017-08-16 15:02:50 -0400

    adding Routing and Navigating pages

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 
+import { EventsListComponent,
+         EventThumbnailComponent,
+         EventService,
+         EventDetailsComponent,
+         CreateEventComponent,
+         EventRouteActivator,
+         EventListResolver
+        }  from './events/index'
+
 import { EventsAppComponent } from './events/events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './nav/routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
+
 
 
 @NgModule({
@@ -33,6 +37,11 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
         EventService,
         ToastrService,
         EventRouteActivator,
+        EventListResolver,
+        {
+            provide: 'canDeactivateCreateEvent',
+            useValue: checkDirtyState
+        }
         
         
     ],
@@ -41,3 +50,10 @@ import { EventRouteActivator } from './events/event-details/event-route-activato
     ]
 })
 export class AppModule {}
+
+function checkDirtyState(component:CreateEventComponent) {
+    if (component.isDirty)
+        return window.confirm('You have not saved this event, do you really want to cancel?')
+   
+
+}
